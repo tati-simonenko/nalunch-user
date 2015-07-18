46f162b88a13e6385f5bdb45a30f76e87f6f627f
Tatyana Simonenko <tatyana.simonenko@gmail.com>
2015-07-18 12:15:00 +0400

    init

---
+++ js/nl-scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$(".nl-btn-menu").click(function(){
+		$(this).css("color":"red");
+		// $(this).parent().find(".nl-table-menu").toggleClass("nl-js-display-none");
+	});
+});
