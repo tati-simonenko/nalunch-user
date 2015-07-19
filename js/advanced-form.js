306b34bb8ae6fbbf202341868a5d6f71d7d1e8b3
Tatyana Simonenko <tatyana.simonenko@gmail.com>
2015-07-19 16:40:13 +0400

    spinner fix

--- js/advanced-form.js
+++ js/advanced-form.js
@@ -172,7 +172,7 @@ $('#my_multi_select3').multiSelect({
 //spinner start
 $('#spinner1').spinner();
 $('#spinner2').spinner({disabled: true});
-$('#spinner3').spinner({value:0, min: 0, max: 10});
+$('.spinner3').spinner({value:0, min: 0, max: 10});
 $('#spinner4').spinner({value:0, step: 5, min: 0, max: 200});
 //spinner end
 
